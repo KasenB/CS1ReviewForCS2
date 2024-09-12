51a6034c2da304bbcbd518c08d4a32e49578eed4
kasenbookbinder <kasen_bookbiner26@milton.edu>
2024-09-12 09:23:39 -0400

    Worder in class through countCups

--- src/JavaBeansAndBytes.java
+++ src/JavaBeansAndBytes.java
@@ -33,13 +33,15 @@ public class JavaBeansAndBytes {
         System.out.println("We were founded in " + yearFounded + ".");
 
         // TODO
+        yearFounded = 2007;
+        System.out.println("Jk. We were actually founded in " + yearFounded + ".");
         // Update the year founded and print the new yearFounded in a sentence
 
         // Calling methods
         randomDiscount();
         // TODO
         // Add parameter to specialOfTheDay (drink of your choice)
-        specialOfTheDay();
+        specialOfTheDay("hot chocolate");
         countCups();
         baristasChoice();
 
@@ -55,15 +57,18 @@ public class JavaBeansAndBytes {
     // Method to generate a random discount
     public void randomDiscount() {
         // TODO
+        int randomDiscount = (int) (Math.random()*31);
         // Make this method generate a random integer discount (0-30%)
         // and print the result.
+        System.out.println("Your random discount of the day is " + randomDiscount + "%!");
     }
 
     // Method with a parameter for the special of the day
-    public void specialOfTheDay() {
+    public void specialOfTheDay(String special) {
         // TODO
         // Make this method accept a parameter representing the day’s special.
         // Print the day's special.
+        System.out.println("Today's special is " + special + ".");
     }
 
     // Method to show loops
@@ -74,12 +79,20 @@ public class JavaBeansAndBytes {
         // Write three different for loops that print the indicated numbers:
 
         // Print 1 to 5
+//        for (int x = 1; x <= 5; x++){
+//            System.out.print(x + " ");
+//        }
 
 
         // Print 2, 5, 8, 11
-
+        for (int x = 2; x <= 11; x = x + 3){
+            System.out.print(x + ", ");
+        }
 
         // Print 8 to 0
+//        for (int x = 8; x >=0; x = x - 1){
+//            System.out.print(x);
+//        }
 
     }
 
